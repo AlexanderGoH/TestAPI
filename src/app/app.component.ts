648b8243a1a68cd14dfc5acf5ea273c427dbdb35
Alex <a.gonzalez5@utp.edu.co>
2024-05-02 23:31:44 -0500

    Página principal añadida al componente home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CardComponent } from './vistas/card/card.component';
-import { NgFor } from '@angular/common';
-import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent, NgFor],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'prograweb';
-  productos = [];
-
-  constructor(private api: ApiService){}
-
-  ngOnInit(){
-    this.api.getAllProducts().subscribe((products: any) => {
-      this.productos = products;
-    });
-  }
+  
 }
