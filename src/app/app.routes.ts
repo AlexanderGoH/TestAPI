87a832629b74e8c5082494b29b73c15a038fc86c
Alex <a.gonzalez5@utp.edu.co>
2024-05-02 23:33:21 -0500

    Adición de las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './vistas/home/home.component';
+import { EditarComponent } from './vistas/editar/editar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'editar', component: EditarComponent},
+];
